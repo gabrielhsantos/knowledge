f5e2a00f5bc1093f4798f8bef0ba7b07f16ea27f
GabrielSantos <ghs.developer@outlook.com>
2019-10-13 09:19:52 -0300

    Creation of categories rules

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -6,4 +6,16 @@ module.exports = app => {
   app.route('/users/:id')
     .put(app.api.user.save)
     .get(app.api.user.getById)
+
+  app.route('/categories')
+    .post(app.api.category.save)
+    .get(app.api.category.get)
+
+  app.route('/categories/tree')
+    .get(app.api.category.getTree)
+
+  app.route('/categories/:id')
+    .put(app.api.category.save)
+    .get(app.api.category.getById)
+    .delete(app.api.category.remove)
 };
