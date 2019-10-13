38b84c05e3f02331ee1dc158e899cad2314447d2
GabrielSantos <ghs.developer@outlook.com>
2019-10-13 07:00:47 -0300

    Get and save user

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ const app = require('express')();
 const consign = require('consign');
 app.db = require('./config/db');
 
-consign()
+consign({ verbose: false })
   .then('./config/middlewares.js')
+  .then('./api/validation.js')
   .then('./api')
   .then('./config/routes.js')
   .into(app);
