66ea3208b704f6e7a5d4a3ba56efed0f4c93f58c
GabrielSantos <ghs.developer@outlook.com>
2019-10-20 18:52:09 -0300

    Use soft delete in user and improvement of env

--- backend/schedule/statsSchedule.js
+++ backend/schedule/statsSchedule.js
@@ -2,9 +2,9 @@ const schedule = require('node-schedule');
 
 module.exports = app => {
   schedule.scheduleJob('*/1 * * * *', async function(){
-    const usersCount = await app.db('users').count('id').first();
-    const categoriesCount = await app.db('categories').count('id').first();
-    const articlesCount = await app.db('articles').count('id').first();
+    const usersCount = await app.db('users').count('id').where({ active: true }).first();
+    const categoriesCount = await app.db('categories').where({ active: true }).count('id').first();
+    const articlesCount = await app.db('articles').where({ active: true }).count('id').first();
 
     const { Stat } = app.api.stat;
 
