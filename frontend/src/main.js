c31a0f99d05a55df93448267d903a698b110436d
Gabriel Santos <ghs.developer@outlook.com>
2019-10-21 17:24:14 -0300

    Component home get stats from backend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,9 @@ import './config/bootstrap';
 
 Vue.config.productionTip = false;
 
+// TEMPORÁRIO
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkdhYnJpZWwgSC4gU2FudG9zIiwiZW1haWwiOiJnaHNAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU3MTY2ODc0NCwiZXhwIjoxODg3MDI4NzQ0fQ.eZKDjOtINA8dPRi5H4l-zZ9x-oGfxrmOj0BCWGPxzeM'
+
 new Vue({
   store,
   router,
