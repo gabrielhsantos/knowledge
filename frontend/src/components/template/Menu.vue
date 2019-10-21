788ca21acfc142f80cbea23a9c1dba3b788f9970
Gabriel Santos <ghs.developer@outlook.com>
2019-10-21 12:55:53 -0300

    Project scaffold

---
+++ frontend/src/components/template/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+  <asside class="menu"></asside>
+</template>
+
+<script>
+export default {
+  name: 'Menu'
+}
+</script>
+
+<style>
+  .menu {
+    grid-area: menu;
+    background: linear-gradient(to right, #232526, #414345);
+
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+  }
+</style>
